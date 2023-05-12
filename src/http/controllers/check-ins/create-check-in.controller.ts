4f807192601a373906b9d2d3e29f333550c48dc8
Leonardo Pinho <leo.o.nardo.sant@gmail.com>
2023-05-12 15:14:26 -0300

    implements /gyms/:gymId/check-ins route

---
+++ src/http/controllers/check-ins/create-check-in.controller.ts
@@ -0,0 +1,32 @@
+import { z } from 'zod'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import CheeckInUseCaseFactory from '../../../use-cases/factories/check-in.usecase.factory'
+
+export async function createCheckIn(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const createCheckInParamsSchema = z.object({
+    gymId: z.string().uuid(),
+  })
+  const createCheckInBodySchema = z.object({
+    userLatitude: z.number(),
+    userLongitude: z.number(),
+  })
+
+  const { gymId } = createCheckInParamsSchema.parse(request.params)
+  const { userLatitude, userLongitude } = createCheckInBodySchema.parse(
+    request.body,
+  )
+
+  try {
+    const checkInUseCase = CheeckInUseCaseFactory.create()
+    await checkInUseCase.execute({
+      userId: request.user.sub,
+      gymId,
+      userLatitude,
+      userLongitude,
+    })
+    return reply.status(201).send()
+  } catch (err) {}
+}
