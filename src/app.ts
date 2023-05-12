f6b5c7b7d5523d6cb52fb8d0aeaf90253aed68de
Leonardo Pinho <leo.o.nardo.sant@gmail.com>
2023-05-12 18:19:23 -0300

    add check in routes

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,10 @@ import { ZodError } from 'zod'
 import { env } from './env'
 import fastifyJwt from '@fastify/jwt'
 import { gymRoutes } from './http/controllers/gyms/routes'
+import { checkInRoutes } from './http/controllers/check-ins/routes'
 
 export const app = fastify()
+app.register(checkInRoutes)
 app.register(userRoutes)
 app.register(gymRoutes)
 app.register(fastifyJwt, { secret: env.JWT_SECRET })
