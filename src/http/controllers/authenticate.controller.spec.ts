b675e9a8688e88e7d96c9c3d2ee48ad14cb267b5
Leonardo Pinho <leo.o.nardo.sant@gmail.com>
2023-05-12 12:38:37 -0300

    add: auth e2e test

--- src/http/controllers/authenticate.controller.spec.ts
+++ src/http/controllers/authenticate.controller.spec.ts
@@ -1,3 +1,32 @@
-import { test } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { agent } from 'supertest'
+import { app } from '../../app'
 
-test('ok', () => {})
+describe('authenticate controller [e2e] ', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+  it('should be able to authenticate after user register', async () => {
+    const registerResponse = await agent(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: 'randompass',
+    })
+    expect(registerResponse.statusCode).toBe(201)
+    const { body, statusCode } = await agent(app.server)
+      .post('/sessions')
+      .send({
+        email: 'johndoe@example.com',
+        password: 'randompass',
+      })
+      .set('accept', 'application/json')
+    console.log(body)
+    expect(statusCode).toBe(200)
+    expect(body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
