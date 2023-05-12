be906eecc2b73271a3f086bfcafc847f0efc55ce
Leonardo Pinho <leo.o.nardo.sant@gmail.com>
2023-05-12 15:19:24 -0300

    new gyms routes

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,7 +1,14 @@
 import { FastifyInstance } from 'fastify'
 import { createGym } from './creeate-gym.controller'
+import { search } from './search-gyms.controller'
+import { getNear } from './get-near-gyms.controller'
+import { verifyJWT } from '../../middlewares/verify-jwt'
 
 export async function gymRoutes(app: FastifyInstance) {
+  app.addHook('onRequest', verifyJWT)
+  app.post('/gyms/search', search)
+  app.post('/gyms/neargyms', getNear)
+
   // Only Authenticated users
   app.post('/gyms', createGym)
 }
