b599d169d3c13d35e67218b03637f6e873f16769
Leonardo Pinho <leo.o.nardo.sant@gmail.com>
2023-05-12 12:38:19 -0300

    register controller e2e test

--- src/http/controllers/register.controller.spec.ts
+++ src/http/controllers/register.controller.spec.ts
@@ -1,3 +1,20 @@
-import { test } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { agent } from 'supertest'
+import { app } from '../../app'
 
-test('ok', () => {})
+describe('Register controller [e2e] ', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+  it('should be able to register without error', async () => {
+    const response = await agent(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: 'randompass',
+    })
+    expect(response.statusCode).toBe(201)
+  })
+})
