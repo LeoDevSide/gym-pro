91718cf03495babaf55f0d5cd708f85f75db90a3
Leonardo Pinho <leo.o.nardo.sant@gmail.com>
2023-05-14 19:42:25 -0300

    add /token/refresh patch route

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,9 +3,12 @@ import { verifyJWT } from '../../middlewares/verify-jwt'
 import { authenticateUser } from './authenticate.controller'
 import { registerUser } from './register.controller'
 import { profile } from './profile.controller'
+import { refresh } from './refresh'
 export async function userRoutes(app: FastifyInstance) {
   app.post('/users', registerUser)
   app.post('/sessions', authenticateUser)
+
+  app.patch('/token/refresh', refresh)
 
   // Only Authenticated users
   app.get('/me', { onRequest: [verifyJWT] }, profile)
